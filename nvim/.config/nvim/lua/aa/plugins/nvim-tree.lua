090b5c58aa66f7390ef88af1dc49ee6df8fbb830
Ashley Austin <austin.ashley.r@gmail.com>
2022-05-24 14:18:25 -0700

    1) Update plugins 2) Update nvim tree config

--- nvim/.config/nvim/lua/aa/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/aa/plugins/nvim-tree.lua
@@ -5,32 +5,12 @@ g.nvim_tree_highlight_opened_files = 2
 
 local ok, nvimtree = aa.safe_require("nvim-tree")
 if not ok then
-	return
+  return
 end
 
 nvimtree.setup({
-	actions = {
-		open_file = {
-			quit_on_open = true,
-			window_picker = {
-				enable = false,
-			},
-		},
-	},
-	view = {
-		width = 45,
-		side = "right",
-	},
-	filters = {
-		custom = { ".DS_Store", "*.un~" },
-	},
-	git = {
-		enable = true,
-		ignore = false,
-	},
-	renderer = {
-		indent_markers = {
-			enable = true,
-		},
-	},
+  view = {
+    width = 45,
+    side = "right",
+  },
 })
