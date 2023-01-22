9fdbd00393ebedd7790823280f7830eaa0a57816
Ashley Austin <austin.ashley.r@gmail.com>
2023-01-21 21:57:49 -0800

    update attempt config

--- nvim/.config/nvim/lua/plugins/attempt.lua
+++ nvim/.config/nvim/lua/plugins/attempt.lua
@@ -16,17 +16,34 @@ end
 Example.run()
 ]]
 
+M.javascript_template = [[
+const hello_world = "hello world!";
+
+const log = () => {
+  console.log(hello_world)
+}
+]]
+
+M.typescript_template = [[
+const hello_world: string = "hello world!";
+
+const log = ():void => {
+  console.log(hello_world)
+}
+]]
+
 M.setup = function()
   attempt.setup {
     autosave = true,
     initial_content = {
       ex = M.elixir_template,
+      js = M.javascript_template,
+      ts = M.typescript_template,
     },
-    ext_options = { 'lua', 'js', 'ex', 'rb', '' },
-    format_opts = { [''] = '[None]', js = 'JavaScript', lua = 'Lua', rb = 'Ruby', ex = 'Elixir' },
+    ext_options = { 'lua', 'js', 'ts', 'ex', '' },
+    format_opts = { [''] = '[None]', js = 'JavaScript', lua = 'Lua', ex = 'Elixir', ts = 'Typescript' },
     run = {
       ex = { 'w', '!elixir %' },
-      rb = { 'w', '!ruby %' },
     },
   }
 
