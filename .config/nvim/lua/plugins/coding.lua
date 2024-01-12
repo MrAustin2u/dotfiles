34cc45b226301c7a1ec893d24ca26fcd1a4d9966
Ashley Austin <austin.ashley.r@gmail.com>
2024-01-11 23:29:54 -0800

    refactor: vim-test

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -459,6 +459,7 @@ return {
     dependencies = { 'benmills/vimux' },
     init = function()
       vim.g['test#strategy'] = 'vimux'
+      vim.g['test#preserve_screen'] = 0
       -- accommodations for Malomo's unusual folder structure on Dash
       vim.cmd [[let test#javascript#jest#file_pattern = '\v(__tests__/.*|(spec|test|__tests__))\.(js|jsx|coffee|ts|tsx)$']]
     end,
