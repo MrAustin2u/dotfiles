65e1942394be09e132f7bf0762caa8817d63fb92
Ashley Austin <austin.ashley.r@gmail.com>
2024-01-26 10:46:36 -0800

    refactor: remove regexplainer plugin causing treesitter issues when neo-tree file explorer is opened from a js/ts/tsx filetype

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -224,23 +224,6 @@ return {
     ,
   },
   --
-  -- Regex
-  --
-  {
-    "bennypowers/nvim-regexplainer",
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-    },
-    config = function()
-      require("regexplainer").setup({
-        auto = true,
-        mappings = {
-          toggle = "gR",
-        },
-      })
-    end,
-  },
-  --
   -- WHICHKEY
   --
   {
