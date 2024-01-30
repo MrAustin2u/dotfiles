4ac7a58818d4deed034fe5f2488a0f2ec3f77651
Ashley Austin <austin.ashley.r@gmail.com>
2024-01-29 16:49:11 -0800

    refactor(lspsettings): remove unnecessary Lua settings

--- .config/nvim/lua/user/lspsettings/lua_ls.lua
+++ .config/nvim/lua/user/lspsettings/lua_ls.lua
@@ -2,7 +2,7 @@ return {
   settings = {
     Lua = {
       workspace = {
-        checkThirdParty = false,
+        checkThirdParty = "Disable",
         library = {
           [vim.fn.expand("$VIMRUNTIME/lua")] = true,
           [vim.fn.stdpath("config") .. "/lua"] = true,
@@ -14,58 +14,9 @@ return {
           spec = "require",
         },
       },
-      completion = {
-        workspaceWord = true,
-        callSnippet = "Both",
-      },
-      hint = {
-        enable = false,
-        arrayIndex = "Disable", -- "Enable" | "Auto" | "Disable"
-        await = true,
-        paramName = "Disable",  -- "All" | "Literal" | "Disable"
-        paramType = true,
-        semicolon = "All",      -- "All" | "SameLine" | "Disable"
-        setType = false,
-      },
-      doc = {
-        privateName = { "^_" },
-      },
-      type = {
-        castNumberToInteger = true,
-      },
       diagnostics = {
         globals = { "vim", "spec" },
-        disable = { "incomplete-signature-doc", "trailing-space" },
-        groupSeverity = {
-          strong = "Warning",
-          strict = "Warning",
-        },
-        groupFileStatus = {
-          ["ambiguity"] = "Opened",
-          ["await"] = "Opened",
-          ["codestyle"] = "None",
-          ["duplicate"] = "Opened",
-          ["global"] = "Opened",
-          ["luadoc"] = "Opened",
-          ["redefined"] = "Opened",
-          ["strict"] = "Opened",
-          ["strong"] = "Opened",
-          ["type-check"] = "Opened",
-          ["unbalanced"] = "Opened",
-          ["unused"] = "Opened",
-        },
         unusedLocalExclude = { "_*" },
-      },
-      telemetry = {
-        enable = false,
-      },
-      format = {
-        enable = true,
-        defaultConfig = {
-          indent_style = "space",
-          indent_size = "2",
-          continuation_indent_size = "2",
-        },
       },
     },
   },
