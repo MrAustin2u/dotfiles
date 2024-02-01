d4e850e2838404f87a7b40ebd7ddd2becec0bedb
Ashley Austin <austin.ashley.r@gmail.com>
2024-02-01 15:15:35 -0800

    chore(treesitter): format file

--- .config/nvim/lua/user/treesitter.lua
+++ .config/nvim/lua/user/treesitter.lua
@@ -1,124 +1,124 @@
 local M = {
-	"nvim-treesitter/nvim-treesitter",
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter-textobjects",
-		"nvim-treesitter/playground",
-		"RRethy/nvim-treesitter-textsubjects",
-		"windwp/nvim-ts-autotag",
-		"JoosepAlviste/nvim-ts-context-commentstring",
-		"IndianBoy42/tree-sitter-just",
-	},
+  "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    "nvim-treesitter/playground",
+    "RRethy/nvim-treesitter-textsubjects",
+    "windwp/nvim-ts-autotag",
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    "IndianBoy42/tree-sitter-just",
+  },
 }
 
 function M.config()
-	require("nvim-treesitter.install").compilers = { "gcc-11" }
-	require("tree-sitter-just").setup({})
-	require("nvim-treesitter.configs").setup({
-		ensure_installed = {
-			"bash",
-			"comment",
-			"css",
-			"csv",
-			"diff",
-			"dockerfile",
-			"eex",
-			"elixir",
-			"erlang",
-			"git_config",
-			"git_rebase",
-			"gitattributes",
-			"gitcommit",
-			"gitignore",
-			"go",
-			"gomod",
-			"gosum",
-			"gowork",
-			"graphql",
-			"heex",
-			"http",
-			"html",
-			"javascript",
-			"jsdoc",
-			"json",
-			"json5",
-			"jsonc",
-			"lua",
-			"markdown",
-			"markdown_inline",
-			"prisma",
-			"python",
-			"regex",
-			"rust",
-			"scss",
-			"sql",
-			"ssh_config",
-			"terraform",
-			"tsx",
-			"typescript",
-			"vim",
-			"vimdoc",
-			"xml",
-			"yaml",
-		},
-		indent = {
-			enable = true,
-		},
-		highlight = {
-			enable = true,
-			use_languagetree = true,
-			additional_vim_regex_highlighting = false,
-		},
-		autotag = {
-			enable = true,
-		},
-		disable = function(lang, buf)
-			local max_filesize = 10 * 1024 -- 10 KB
-			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-			if ok and stats and stats.size > max_filesize then
-				return true
-			end
-		end,
-		textobjects = {
-			select = {
-				enable = true,
-				lookahead = true,
-				keymaps = {
-					["af"] = "@function.outer",
-					["if"] = "@function.inner",
-					["am"] = "@class.outer",
-					["im"] = "@class.inner",
-					["ac"] = "@comment.outer",
-					["as"] = "@statement.outer",
-				},
-			},
-			move = {
-				enable = true,
-				set_jumps = true, -- whether to set jumps in the jumplist
-				goto_next_start = {
-					["]m"] = "@function.outer",
-					["]]"] = "@class.outer",
-				},
-				goto_next_end = {
-					["]M"] = "@function.outer",
-					["]["] = "@class.outer",
-				},
-				goto_previous_start = {
-					["[m"] = "@function.outer",
-					["[["] = "@class.outer",
-				},
-				goto_previous_end = {
-					["[M"] = "@function.outer",
-					["[]"] = "@class.outer",
-				},
-			},
-		},
-		textsubjects = {
-			enable = true,
-			keymaps = {
-				["."] = "textsubjects-smart",
-			},
-		},
-	})
+  require("nvim-treesitter.install").compilers = { "gcc-11" }
+  require("tree-sitter-just").setup({})
+  require("nvim-treesitter.configs").setup({
+    ensure_installed = {
+      "bash",
+      "comment",
+      "css",
+      "csv",
+      "diff",
+      "dockerfile",
+      "eex",
+      "elixir",
+      "erlang",
+      "git_config",
+      "git_rebase",
+      "gitattributes",
+      "gitcommit",
+      "gitignore",
+      "go",
+      "gomod",
+      "gosum",
+      "gowork",
+      "graphql",
+      "heex",
+      "http",
+      "html",
+      "javascript",
+      "jsdoc",
+      "json",
+      "json5",
+      "jsonc",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "prisma",
+      "python",
+      "regex",
+      "rust",
+      "scss",
+      "sql",
+      "ssh_config",
+      "terraform",
+      "tsx",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "xml",
+      "yaml",
+    },
+    indent = {
+      enable = true,
+    },
+    highlight = {
+      enable = true,
+      use_languagetree = true,
+      additional_vim_regex_highlighting = false,
+    },
+    autotag = {
+      enable = true,
+    },
+    disable = function(lang, buf)
+      local max_filesize = 10 * 1024 -- 10 KB
+      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+      if ok and stats and stats.size > max_filesize then
+        return true
+      end
+    end,
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true,
+        keymaps = {
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          ["am"] = "@class.outer",
+          ["im"] = "@class.inner",
+          ["ac"] = "@comment.outer",
+          ["as"] = "@statement.outer",
+        },
+      },
+      move = {
+        enable = true,
+        set_jumps = true, -- whether to set jumps in the jumplist
+        goto_next_start = {
+          ["]m"] = "@function.outer",
+          ["]]"] = "@class.outer",
+        },
+        goto_next_end = {
+          ["]M"] = "@function.outer",
+          ["]["] = "@class.outer",
+        },
+        goto_previous_start = {
+          ["[m"] = "@function.outer",
+          ["[["] = "@class.outer",
+        },
+        goto_previous_end = {
+          ["[M"] = "@function.outer",
+          ["[]"] = "@class.outer",
+        },
+      },
+    },
+    textsubjects = {
+      enable = true,
+      keymaps = {
+        ["."] = "textsubjects-smart",
+      },
+    },
+  })
 end
 
 return M
