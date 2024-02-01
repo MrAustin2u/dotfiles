7e757899f713661ca72e92a6fefff761b3fdfe8d
Ashley Austin <austin.ashley.r@gmail.com>
2024-01-31 17:30:44 -0800

    feature(hammerspoon): add keymap for Notion Calendar

--- .hammerspoon/keymaps.lua
+++ .hammerspoon/keymaps.lua
@@ -12,7 +12,7 @@ end
 hs.hotkey.bind({ "command" }, ".", open_app("kitty", "/Applications/Kitty.app"))
 hs.hotkey.bind({ "command" }, "/", open_app("WezTerm", "/Applications/WezTerm.app"))
 -- Ctrl + 1,2,3 is used to switch to different desktops
+hs.hotkey.bind({ "control" }, "n", open_app("Arc", "/Applications/Notion Calendar.app"))
 hs.hotkey.bind({ "control" }, "4", open_app("Arc", "/Applications/Arc.app"))
 hs.hotkey.bind({ "control" }, "5", open_app("Dash", "/Applications/Dash.app"))
-hs.hotkey.bind({ "control" }, "6", open_app("Slack", "/Applications/Slack.app"))
 hs.hotkey.bind({ "control" }, "6", open_app("Slack", "/Applications/DataGrip.app"))
