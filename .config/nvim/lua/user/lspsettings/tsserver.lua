90df31471019378fa8ec49d7c34b318158a1218a
Ashley Austin <austin.ashley.r@gmail.com>
2024-02-25 17:31:28 -0800

    refactor: lsp tsserver

--- .config/nvim/lua/user/lspsettings/tsserver.lua
+++ .config/nvim/lua/user/lspsettings/tsserver.lua
@@ -1,56 +1,22 @@
 return {
-	keys = {
-		{
-			"<leader>co",
-			function()
-				vim.lsp.buf.code_action({
-					apply = true,
-					context = {
-						only = { "source.organizeImports.ts" },
-						diagnostics = {},
-					},
-				})
-			end,
-			desc = "Organize Imports",
-		},
-	},
-	settings = {
-		typescript = {
-			format = {
-				indentSize = vim.o.shiftwidth,
-				convertTabsToSpaces = vim.o.expandtab,
-				tabSize = vim.o.tabstop,
-			},
-			inlayHints = {
-				includeInlayParameterNameHints = "all",
-				includeInlayFunctionLikeReturnTypeHints = true,
-				includeInlayFunctionParameterTypeHints = true,
-				includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-				includeInlayPropertyDeclarationTypeHints = true,
-				includeInlayVariableTypeHints = true,
-				includeInlayVariableTypeHintsWhenTypeMatchesName = true,
-				includeInlayEnumMemberValueHints = true,
-			},
-		},
-		javascript = {
-			format = {
-				indentSize = vim.o.shiftwidth,
-				convertTabsToSpaces = vim.o.expandtab,
-				tabSize = vim.o.tabstop,
-			},
-			inlayHints = {
-				includeInlayParameterNameHints = "all",
-				includeInlayFunctionLikeReturnTypeHints = true,
-				includeInlayFunctionParameterTypeHints = true,
-				includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-				includeInlayPropertyDeclarationTypeHints = true,
-				includeInlayVariableTypeHints = true,
-				includeInlayVariableTypeHintsWhenTypeMatchesName = true,
-				includeInlayEnumMemberValueHints = true,
-			},
-		},
-		completions = {
-			completeFunctionCalls = true,
-		},
-	},
+  keys = {
+    {
+      "<leader>co",
+      function()
+        vim.lsp.buf.code_action({
+          apply = true,
+          context = {
+            only = { "source.organizeImports.ts" },
+            diagnostics = {},
+          },
+        })
+      end,
+      desc = "Organize Imports",
+    },
+  },
+  settings = {
+    completions = {
+      completeFunctionCalls = true,
+    },
+  },
 }
