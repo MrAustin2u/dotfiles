88d60adbb9e86c8c846461e158e6fe4b2d46d8dc
Ashley Austin <austin.ashley.r@gmail.com>
2024-04-22 23:21:02 -0700

    refactor: fzf

--- .config/zsh/fzf.zsh
+++ .config/zsh/fzf.zsh
@@ -1,5 +1,4 @@
-# fzf stuff
-export FZF_DEFAULT_COMMAND='fd --type f --follow --hidden --color=always --exclude .git --exclude node_modules --exclude vendor --exclude build --exclude _build --exclude bundle --exclude Godeps'
+export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
 # add support for Ansi for fd color
@@ -12,3 +11,16 @@ export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
   --color=fg+:#c5cdd9,bg+:#000000,hl+:#5dbbc1
   --color=info:#88909f,prompt:#ec7279,pointer:#d38aea
   --color=marker:#a0c980,spinner:#ec7279,header:#5dbbc1'
+
+  # Use fd for listing path candiates.
+  # - The first argument to the function ($1) is the base path to start traversal
+  _fzf_compgen_path() {
+    fd --hidden --exclude .git . "$1"
+  }
+
+  # Use fd to generate the list for directory complettion
+  _fzf_compgen_dir() {
+    fd --type=d --hidden --exclude .git . "$1"
+  }
+
+  source ~/fzf-git.sh/fzf-git.sh
