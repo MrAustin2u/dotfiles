05c09df91305d7c459f337da31050c254b781189
Ashley Austin <austin.ashley.r@gmail.com>
2024-05-23 21:46:16 -0700

    refactor(harpoon): update to harpoon2

--- .config/nvim/lua/user/harpoon.lua
+++ .config/nvim/lua/user/harpoon.lua
@@ -1,41 +1,48 @@
 local M = {
   "ThePrimeagen/harpoon",
+  branch = "harpoon2",
   event = "VeryLazy",
   dependencies = {
-    { "nvim-lua/plenary.nvim" },
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
   },
 }
 
 function M.config()
-  require("harpoon").setup({
-    tabline = true,
-    tabline_prefix = "   ",
-    tabline_suffix = "   ",
-    menu = {
-      width = vim.api.nvim_win_get_width(0) - 100,
-    }
-  })
+  local harpoon = require("harpoon")
+
+  harpoon:setup()
+
+  -- basic telescope configuration
+  local conf = require("telescope.config").values
+  local function toggle_telescope(harpoon_files)
+    local file_paths = {}
+    for _, item in ipairs(harpoon_files.items) do
+      table.insert(file_paths, item.value)
+    end
+
+    require("telescope.pickers").new({}, {
+      prompt_title = "Harpoon",
+      finder = require("telescope.finders").new_table({
+        results = file_paths,
+      }),
+      previewer = conf.file_previewer({}),
+      sorter = conf.generic_sorter({}),
+    }):find()
+  end
 
   -- mappings
-  local ui = require("harpoon.ui")
-  local opts = { noremap = true, silent = true }
-  vim.keymap.set("n", "<leader>a", "<cmd>lua require('user.harpoon').mark_file()<cr>", opts)
-  vim.keymap.set("n", "<TAB>", ui.nav_next, opts)
-  vim.keymap.set("n", "<S-TAB>", ui.nav_prev, opts)
-  vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, opts)
-
-
-  -- make the tabline cleaner
-  vim.cmd('highlight! HarpoonInactive guibg=NONE guifg=#63698c')
-  vim.cmd('highlight! HarpoonActive guibg=NONE guifg=white')
-  vim.cmd('highlight! HarpoonNumberActive guibg=NONE guifg=#7aa2f7')
-  vim.cmd('highlight! HarpoonNumberInactive guibg=NONE guifg=#7aa2f7')
-  vim.cmd('highlight! TabLineFill guibg=NONE guifg=white')
-end
-
-function M.mark_file()
-  require("harpoon.mark").add_file()
-  vim.notify "󱡅  marked file"
+  vim.keymap.set("n", "<leader>a", function()
+      harpoon:list():add()
+      vim.notify "󱡅  marked file"
+    end,
+    { noremap = true, silent = true, desc = "[harpoon] add file" })
+  vim.keymap.set("n", "<TAB>", function() harpoon:list():next() end,
+    { noremap = true, silent = true, desc = "[harpoon] next buffer" })
+  vim.keymap.set("n", "<S-TAB>", function() harpoon:list():prev() end,
+    { noremap = true, silent = true, desc = "[harpoon] previous buffer" })
+  vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
+    { noremap = true, silent = true, desc = "[harpoon] Open window" })
 end
 
 return M
