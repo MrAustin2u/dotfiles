1564da07c54c991df539d2e3e0953aaced0a8b45
Ashley Austin <austin.ashley.r@gmail.com>
2024-05-26 01:47:21 -0700

    fix(iex): elixir 1.16.0-otp-26 doesn't support ansi

--- .iex.exs
+++ .iex.exs
@@ -1,13 +1,5 @@
 import_if_available(Ecto.Query)
 
-defmodule IExHelpers do
-  def whats_this?(term) when is_nil(term), do: "Type: Nil"
-  def whats_this?(term) when is_binary(term), do: "Type: Binary"
-  def whats_this?(term) when is_boolean(term), do: "Type: Boolean"
-  def whats_this?(term) when is_atom(term), do: "Type: Atom"
-  def whats_this?(_term), do: "Type: Unknown"
-end
-
 IEx.configure(
   inspect: [limit: :infinity, pretty: true],
   colors: [
@@ -27,10 +19,7 @@ IEx.configure(
   ],
   default_prompt:
     [
-      # ANSI CHA, move cursor to column 1
-      "\e[G",
       :light_magenta,
-      # plain string
       "🧪 iex",
       ">",
       :white,
