a1721b51e384338eaa465a37fcae02919cfd1a24
Ashley Austin <austin.ashley.r@gmail.com>
2024-05-29 22:59:37 -0700

    refactor: lualine

--- .config/nvim/lua/user/lualine.lua
+++ .config/nvim/lua/user/lualine.lua
@@ -1,40 +1,41 @@
 local M = {
-	"nvim-lualine/lualine.nvim",
-	opts = {
-		options = {
-			theme = "auto",
-			disabled_filetypes = {
-				"TelescopePrompt",
-				"TelescopeResults",
-				statusline = { "dashboard", "alpha", "starter" },
-			},
-			component_separators = "|",
-			section_separators = { left = "", right = "" },
-			globalstatus = true,
-		},
-		sections = {
-			lualine_a = {
-				{ "mode", separator = { left = "" }, right_padding = 2 },
-			},
-			lualine_b = { "branch", "diff", "diagnostics" },
-			lualine_c = { { "filename", path = 1 } },
-			lualine_x = {
-				{
-					require("lazy.status").updates,
-					cond = require("lazy.status").has_updates,
-					color = { bg = "none", fg = "#ff966c" },
-				},
-			},
-			lualine_z = {
-				{ "location", separator = { right = "" }, left_padding = 2 },
-			},
-		},
-		extensions = { "neo-tree", "lazy" },
-	},
+  "nvim-lualine/lualine.nvim",
+  opts = {
+    options = {
+      theme = "auto",
+      disabled_filetypes = {
+        "TelescopePrompt",
+        "TelescopeResults",
+        statusline = { "dashboard", "alpha", "starter" },
+      },
+      component_separators = "|",
+      section_separators = { left = "", right = "" },
+      globalstatus = true,
+    },
+    sections = {
+      lualine_a = {
+        { "mode", separator = { left = "" }, right_padding = 2 },
+      },
+      lualine_b = { "branch", "diff", "diagnostics" },
+      lualine_c = { 'filename', function() return require('lsp-progress').progress() end },
+      lualine_x = {
+        {
+          require("lazy.status").updates,
+          cond = require("lazy.status").has_updates,
+          color = { bg = "none", fg = "#ff966c" },
+        },
+      },
+      lualine_y = { 'filetype', 'progress' },
+      lualine_z = {
+        { "location", separator = { right = "" }, left_padding = 2 },
+      },
+    },
+    extensions = { "neo-tree", "lazy" },
+  },
 }
 
 function M.config(_, opts)
-	require("lualine").setup(opts)
+  require("lualine").setup(opts)
 end
 
 return M
