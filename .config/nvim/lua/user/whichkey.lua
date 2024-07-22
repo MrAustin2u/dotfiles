3cd1c8f7667fc2ca1db7a55d76b2e66d2febdb56
Ashley Austin <austin.ashley.r@gmail.com>
2024-07-22 11:12:59 -0700

    refactor: whichkey config

--- .config/nvim/lua/user/whichkey.lua
+++ .config/nvim/lua/user/whichkey.lua
@@ -1,31 +1,52 @@
 local M = {
   "folke/which-key.nvim",
   event = "VeryLazy",
+  opts_extend = { "spec" },
+  opts = {
+    defaults = {},
+    spec = {
+      {
+        mode = { "n", "v" },
+        {
+          "<leader>b",
+          group = "buffer",
+          expand = function()
+            return require("which-key.extras").expand.buf()
+          end,
+        },
+        { "<leader>q", group = "quit/session" },
+        { "<leader>s", group = "search" },
+        { "<leader>sn", group = "noice" },
+        { "<leader>u", group = "ui" },
+        { "<leader>t", group = "test" },
+        { "<leader>w", group = "windows" },
+        { "<leader>x", group = "diagnostics/quickfix" },
+        { "[", group = "prev" },
+        { "]", group = "next" },
+        { "g", group = "goto" },
+        { "gz", group = "surround" },
+        { "<leader><tab>", group = "tabs" },
+        { "<leader>c", group = "code" },
+        { "<leader>f", group = "file/find" },
+        { "<leader>g", group = "git" },
+        { "<leader>gh", group = "hunks" },
+      },
+    },
+  },
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show { global = false }
+      end,
+      desc = "Buffer Keymaps (which-key)",
+    },
+  },
 }
 
-function M.config()
+function M.config(_, opts)
   local wk = require "which-key"
-
-  wk.add {
-    mode = { "n", "v" },
-    { "<leader>q", group = "quit/session" },
-    { "<leader>s", group = "search" },
-    { "<leader>sn", group = "noice" },
-    { "<leader>u", group = "ui" },
-    { "<leader>t", group = "test" },
-    { "<leader>w", group = "windows" },
-    { "<leader>x", group = "diagnostics/quickfix" },
-    { "[", group = "prev" },
-    { "]", group = "next" },
-    { "g", group = "goto" },
-    { "gz", group = "surround" },
-    { "<leader><tab>", group = "tabs" },
-    { "<leader>b", group = "buffer" },
-    { "<leader>c", group = "code" },
-    { "<leader>f", group = "file/find" },
-    { "<leader>g", group = "git" },
-    { "<leader>gh", group = "hunks" },
-  }
+  wk.setup(opts)
 end
 
 return M
