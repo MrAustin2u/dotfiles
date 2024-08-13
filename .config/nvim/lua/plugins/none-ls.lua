ca87faee91c8b88608fe4d99c17ade6b076c29d6
Ashley Austin <austin.ashley.r@gmail.com>
2024-08-13 02:11:58 -0700

    feat: add none-ls

---
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -0,0 +1,29 @@
+return {
+  "nvimtools/none-ls.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
+  config = function()
+    local null_ls = require "null-ls"
+
+    null_ls.setup {
+      sources = {
+        -- code actions
+        null_ls.builtins.code_actions.gitsigns,
+        -- linters
+        require "none-ls.diagnostics.eslint_d",
+        null_ls.builtins.diagnostics.actionlint,
+        null_ls.builtins.diagnostics.ansiblelint,
+        null_ls.builtins.diagnostics.credo,
+        null_ls.builtins.diagnostics.yamllint,
+        null_ls.builtins.diagnostics.zsh,
+        -- formatters
+        null_ls.builtins.formatting.erlfmt,
+        null_ls.builtins.formatting.just,
+        null_ls.builtins.formatting.prettierd,
+        null_ls.builtins.formatting.terraform_fmt,
+      },
+    }
+  end,
+}
