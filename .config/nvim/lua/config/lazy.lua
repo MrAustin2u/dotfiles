cb4b25d537d1e047bb90c5904946659f6e8ee10a
Ashley Austin <austin.ashley.r@gmail.com>
2024-08-19 22:06:44 -0700

    refactor: lazy config

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -15,6 +15,9 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 -- Setup lazy.nvim
 require("lazy").setup {
   spec = {
@@ -25,11 +28,8 @@ require("lazy").setup {
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "tokyonight", "habamax" } },
-  -- automatically check for plugin updates
-  checker = {
-    enabled = true,
-    notify = false,
-  },
+  checker = { notify = false },
+  change_detection = { enabled = false },
   performance = {
     rtp = {
       disabled_plugins = {
