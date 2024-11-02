fbc4553b2dabcab227a8b94b2ba830eb1dbd1d94
Ashley Austin <austin.ashley.r@gmail.com>
2024-11-02 03:11:36 -0700

    refactor(nvim): update oil config

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,10 +1,11 @@
 return {
   "stevearc/oil.nvim",
-  -- we need this to work when running nvim .
-  -- it's ok though because this only adds ~1.5ms to load time
   event = { "VimEnter" },
   keys = require("config.keymaps").oil_mappings(),
   opts = {
+    default_file_explorer = true,
+    delete_to_trash = true,
+    skip_confirm_for_simple_edits = true,
     columns = { "icon" },
     keymaps = {
       ["<C-c>"] = false,
@@ -13,6 +14,17 @@ return {
     },
     view_options = {
       show_hidden = true,
+      natural_order = true,
+      is_always_hidden = function(name, _bufnr)
+        return name == ".." or name == ".git"
+      end,
+    },
+
+    float = {
+      -- Padding around the floating window
+      padding = 8,
+      max_width = 120,
+      max_height = 120,
     },
   },
   -- Optional dependencies
