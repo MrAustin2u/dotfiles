3d00f958c71cea8e051be404a7c0d21bf65e6039
Ashley Austin <austin.ashley.r@gmail.com>
2024-11-02 03:09:33 -0700

    refactor(nvim): use magazine.nvim instead of nvim-cmp

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,5 +1,6 @@
 return {
-  "hrsh7th/nvim-cmp",
+  "iguanacucumber/magazine.nvim",
+  name = "nvim-cmp", -- Otherwise highlighting gets messed up
   event = "InsertEnter",
   dependencies = {
     {
