fc88ec2a36481c01b2a6b93cc5bc1ed8efb4ecbb
Ashley Austin <austin.ashley.r@gmail.com>
2024-11-10 02:08:13 -0800

    refactor(nvim): update telescope config

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -14,19 +14,86 @@ return {
   opts = function()
     return {
       defaults = {
+        -- layout_config = {
+        --   center = { width = 0.8 },
+        -- },
+        entry_prefix = "  ",
+        prompt_prefix = "   ",
+        selection_caret = "  ",
+        color_devicons = true,
+        path_display = { "smart" },
+        dynamic_preview_title = true,
         layout_config = {
-          center = { width = 0.8 },
+          horizontal = {
+            height = 0.95,
+            preview_width = 0.55,
+            prompt_position = "bottom",
+            width = 0.9,
+          },
+          center = {
+            anchor = "N",
+            width = 0.9,
+            preview_cutoff = 10,
+          },
+          vertical = {
+            height = 0.9,
+            preview_height = 0.3,
+            width = 0.9,
+            preview_cutoff = 10,
+            prompt_position = "bottom",
+          },
         },
+
+        -- Searching
+        set_env = { COLORTERM = "truecolor" },
+        file_ignore_patterns = {
+          ".git/",
+          "%.jpg",
+          "%.jpeg",
+          "%.png",
+          "%.svg",
+          "%.otf",
+          "%.ttf",
+          "%.lock",
+          "__pycache__",
+          "%.sqlite3",
+          "%.ipynb",
+          "vendor",
+          "node_modules",
+          "dotbot",
+        },
+        file_sorter = require("telescope.sorters").get_fuzzy_file,
       },
       pickers = {
         find_files = {
+          hidden = true,
           theme = "ivy",
           prompt_prefix = " ",
           previewer = false,
+          find_command = {
+            "fd",
+            "--type",
+            "f",
+            "--strip-cwd-prefix",
+            "--hidden",
+            "--follow",
+            "--ignore-file",
+            "~/.gitignore",
+          },
+        },
+        grep_string = {
+          additional_args = { "--hidden" },
         },
         live_grep = {
           prompt_prefix = " ",
+          additional_args = { "--hidden" },
         },
+      },
+      fzf = {
+        fuzzy = true,
+        override_generic_sorter = true,
+        override_file_sorter = true,
+        case_mode = "smart_case",
       },
       extensions = {
         persisted = {
