7ffa3c5411f2de31ca44de07c8a806c20f0baa3e
Ashley Austin <austin.ashley.r@gmail.com>
2024-11-19 01:02:02 -0800

    refactor(nvim): update snacks config

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -2,6 +2,23 @@ return {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
-  opts = {},
+  opts = {
+    notifier = {
+      enabled = true,
+      timeout = 3000,
+    },
+    git = { enabled = true },
+    lazygit = { enabled = true },
+    words = { enabled = true },
+    terminal = { enabled = true },
+    bufdelete = { enabled = true },
+    rename = { enabled = true },
+    statuscolumn = { enabled = true },
+    styles = {
+      notification = {
+        wo = { wrap = true }, -- Wrap notifications
+      },
+    },
+  },
   keys = require("config.keymaps").snacks_mappings,
 }
