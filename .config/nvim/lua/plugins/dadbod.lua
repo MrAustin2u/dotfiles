af9c316fe09c5dfde6c1adcc8afd137be2f93529
Ashley Austin <austin.ashley.r@gmail.com>
2024-12-01 18:30:47 -0800

    refactor: fix query save location

--- .config/nvim/lua/plugins/dadbod.lua
+++ .config/nvim/lua/plugins/dadbod.lua
@@ -26,7 +26,7 @@ return {
       vim.g.db_ui_use_nerd_fonts = true
       vim.g.db_ui_execute_on_save = false
       vim.g.db_ui_disable_mappings = true
-      vim.g.db_ui_save_location = "./dorians_queries"
+      vim.g.db_ui_save_location = "$HOME/code/sql/queries"
       vim.g.db_ui_use_nvim_notify = true
     end,
   },
