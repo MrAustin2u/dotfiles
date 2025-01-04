dedff6ae3eb62f1f2902a4d2fb7cfea38778c242
Ashley Austin <austin.ashley.r@gmail.com>
2025-01-04 14:32:21 -0800

    refactor(hammerspoon): update keymaps and add spoons

---
+++ .hammerspoon/aerospace.lua
@@ -0,0 +1,32 @@
+Swipe = hs.loadSpoon "Swipe"
+
+local AEROSPACE = "/opt/homebrew/bin/aerospace"
+
+local function aerospaceExec(cmd)
+  os.execute("nohup " .. AEROSPACE .. " " .. cmd .. " &")
+end
+
+-- use three finger swipe to switch workspace
+local current_id, threshold
+Swipe:start(3, function(direction, distance, id)
+  if id == current_id then
+    if distance > threshold then
+      threshold = math.huge -- only trigger once per swipe
+      if direction == "left" then
+        aerospaceExec "workspace --wrap-around next"
+      elseif direction == "right" then
+        aerospaceExec "workspace --wrap-around prev"
+      end
+    end
+  else
+    current_id = id
+    threshold = 0.04 -- swipe distance > 4% of trackpad
+  end
+end)
+
+hs.hotkey.bind({ "ctrl" }, "right", function()
+  aerospaceExec "workspace --wrap-around next"
+end)
+hs.hotkey.bind({ "ctrl" }, "left", function()
+  aerospaceExec "workspace --wrap-around prev"
+end)
--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,7 +1,7 @@
 local function reload_config(files)
   local doReload = false
   for _, file in pairs(files) do
-    if file:sub(-4) == '.lua' then
+    if file:sub(-4) == ".lua" then
       doReload = true
     end
   end
@@ -11,11 +11,13 @@ local function reload_config(files)
 end
 
 -- reload config every time it changes
-local config_watcher = hs.pathwatcher.new('~/.hammerspoon/', reload_config)
+local config_watcher = hs.pathwatcher.new("~/.hammerspoon/", reload_config)
 config_watcher:start()
 
 -- load SpoonInstall
--- hs.loadSpoon('SpoonInstall')
+hs.loadSpoon "SpoonInstall"
 
 -- import modules
-require('keymaps')
+require "keymaps"
+require "border"
+require "keymaps"
