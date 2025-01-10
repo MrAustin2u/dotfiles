8d97701ae20a1738a0ab09d8ccdee12085738e98
Ashley Austin <austin.ashley.r@gmail.com>
2025-01-09 19:36:57 -0800

    style: formatting

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -57,7 +57,7 @@ return {
       },
       sections = {
         { section = "header" },
-        { section = "keys",   gap = 1, padding = 1 },
+        { section = "keys", gap = 1, padding = 1 },
         { section = "startup" },
       },
     },
