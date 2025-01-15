2097835ec2c8c25c94aa9eeb00463c4862993524
Ashley Austin <austin.ashley.r@gmail.com>
2025-01-14 23:29:01 -0800

    refactor(hammerspoon): update keymaps

--- .hammerspoon/keymaps.lua
+++ .hammerspoon/keymaps.lua
@@ -12,6 +12,7 @@ end
 hs.hotkey.bind({ "command" }, ".", open_app("Ghostty", "/Applications/Ghostty.app"))
 -- Ctrl + 1,2,3 is used to switch to different desktops
 hs.hotkey.bind({ "command" }, "4", open_app("Arc", "/Applications/Arc.app"))
-hs.hotkey.bind({ "command" }, "5", open_app("Dash", "/Applications/Dash.app"))
-hs.hotkey.bind({ "command" }, "6", open_app("Slack", "/Applications/Slack.app"))
-hs.hotkey.bind({ "command" }, "7", open_app("DataGrip", "/Applications/DataGrip.app"))
+hs.hotkey.bind({ "command" }, "5", open_app("Firefox", "/Applications/Firefox.app"))
+hs.hotkey.bind({ "command" }, "6", open_app("Dash", "/Applications/Dash.app"))
+hs.hotkey.bind({ "command" }, "7", open_app("Slack", "/Applications/Slack.app"))
+hs.hotkey.bind({ "command" }, "8", open_app("DataGrip", "/Applications/DataGrip.app"))
