c37dc5266cd1c1ee1d4671592922ca13dbbd3cd9
Ashley Austin <austin.ashley.r@gmail.com>
2025-01-17 22:10:51 -0800

    refactor(nvim): update output-panel config

--- .config/nvim/lua/plugins/output-panel.lua
+++ .config/nvim/lua/plugins/output-panel.lua
@@ -2,7 +2,11 @@ return {
   "mhanberg/output-panel.nvim",
   version = "*",
   event = "VeryLazy",
-  config = true,
+  config = function()
+    require("output_panel").setup {
+      max_buffer_size = 5000, -- default
+    }
+  end,
   cmd = { "OutputPanel" },
   keys = {
     {
