8a97c7ce78de311864c09f8c7491ca4f7bfcbed1
Ashley Austin <austin.ashley.r@gmail.com>
2025-01-21 22:36:59 -0800

    refactor(nvim): add vim-test keys

--- .config/nvim/lua/plugins/vim-test.lua
+++ .config/nvim/lua/plugins/vim-test.lua
@@ -3,7 +3,32 @@ return {
   keys = require("config.keymaps").vim_test_mappings(),
   dependencies = {
     {
-      "benmills/vimux",
+      "preservim/vimux",
+      keys = {
+        { "<leader>rr", "<CMD>VimuxPromptCommand<CR>",       desc = "run a command (prompt)" },
+        { "<leader>r.", "<CMD>VimuxRunLastCommand<CR>",      desc = "run the last run command" },
+        { "<leader>rc", "<CMD>VimuxClearTerminalScreen<CR>", desc = "clear the current run terminal" },
+        { "<leader>rq", "<CMD>VimuxCloseRunner<CR>",         desc = "close the runner" },
+        { "<leader>r?", "<CMD>VimuxInspectRunner<CR>",       desc = "inspect the runner" },
+        { "<leader>r!", "<CMD>VimuxInterruptRunner<CR>",     desc = "interrupt the runner (bang'er)" },
+        { "<leader>rz", "<CMD>VimuxZoomRunner<CR>",          desc = "zoom the runner" },
+        {
+          "<C-c><C-c>",
+          function()
+            -- yank text into v register
+            if vim.api.nvim_get_mode()["mode"] == "n" then
+              vim.cmd 'normal vip"vy'
+            else
+              vim.cmd 'normal "vy'
+            end
+
+            -- construct command with v register as command to send
+            -- vim.cmd(string.format('call VimuxRunCommand("%s")', vim.trim(vim.fn.getreg('v'))))
+            vim.cmd "call VimuxRunCommand(@v)"
+          end,
+          desc = "run command under cursor",
+        },
+      },
       config = function()
         vim.g["VimuxOrientation"] = "h"
         vim.g["VimuxHeight"] = "35%"
