b49577f6b5fb2bd26ee74a25b87fb47bdddb737b
Ashley Austin <austin.ashley.r@gmail.com>
2025-02-20 23:43:58 -0800

    refactor(nvim): update colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -39,6 +39,16 @@ return {
         which_key = true,
       },
     },
+    specs = {
+      {
+        "akinsho/bufferline.nvim",
+        opts = function(_, opts)
+          if (vim.g.colors_name or ""):find("catppuccin") then
+            opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
+          end
+        end,
+      },
+    },
   },
   {
     "folke/tokyonight.nvim",
