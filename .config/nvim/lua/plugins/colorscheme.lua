13a32b1f7d98f56a80b1f6ce0839a3a533629dbf
Ashley Austin <austin.ashley.r@gmail.com>
2025-02-22 16:21:19 -0800

    refactor(nvim): update tokyonight config

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -43,7 +43,7 @@ return {
       {
         "akinsho/bufferline.nvim",
         opts = function(_, opts)
-          if (vim.g.colors_name or ""):find("catppuccin") then
+          if (vim.g.colors_name or ""):find "catppuccin" then
             opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
           end
         end,
@@ -57,22 +57,6 @@ return {
     opts = {
       plugins = { markdown = true },
       tokyonight_dark_float = false,
-      on_highlights = function(hl, c)
-        hl.CursorLineNr = {
-          fg = c.orange,
-          bold = true,
-        }
-        hl.LineNrAbove = { fg = c.fg_gutter }
-        hl.LineNrBelow = { fg = c.fg_gutter }
-        local prompt = "#2d3149"
-        hl.TelescopeNormal = { bg = c.bg_dark, fg = c.fg_dark }
-        hl.TelescopeBorder = { bg = c.bg_dark, fg = c.bg_dark }
-        hl.TelescopePromptNormal = { bg = prompt }
-        hl.TelescopePromptBorder = { bg = prompt, fg = prompt }
-        hl.TelescopePromptTitle = { bg = c.fg_gutter, fg = c.orange }
-        hl.TelescopePreviewTitle = { bg = c.bg_dark, fg = c.bg_dark }
-        hl.TelescopeResultsTitle = { bg = c.bg_dark, fg = c.bg_dark }
-      end,
       dim_inactive = true,
       hide_inactive_statusline = true,
       sidebars = {
@@ -85,18 +69,13 @@ return {
         "Outline",
       },
       style = "moon",
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
       transparent = true,
     },
     config = function(_, opts)
       require("tokyonight").setup(opts)
 
-      vim.cmd [[colorscheme tokyonight]]
+      vim.cmd [[colorscheme tokyonight-moon]]
       vim.cmd [[hi TabLine guibg=NONE guifg=NONE]]
-      vim.cmd [[hi WinBar guibg=NONE guifg=NONE]]
       vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
     end,
   },
