52867c5539525de9d0e90cada6c0cfdd45afd7a3
Ashley Austin <austin.ashley.r@gmail.com>
2025-02-23 22:47:55 -0800

    refactor(nvim): remove local-highlight

--- .config/nvim/lua/plugins/local-highlight.lua
+++ .config/nvim/lua/plugins/local-highlight.lua
@@ -1,8 +1,6 @@
-return {
-  "tzachar/local-highlight.nvim",
-  config = function()
-    vim.cmd [[hi TSDefinitionUsage guibg=#565f89]]
+-- vim.cmd [[hi TSDefinitionUsage guibg=#565f89]]
 
-    require("local-highlight").setup()
-  end,
+return {
+  -- "tzachar/local-highlight.nvim",
+  -- config = true,
 }
