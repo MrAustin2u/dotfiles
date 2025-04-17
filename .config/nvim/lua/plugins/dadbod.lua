ebaf606a71eeab7b573abac9bcc5a707f052b8c0
Ashley Austin <austin.ashley.r@gmail.com>
2025-04-16 20:39:44 -0700

    refactor(nvim): update dadbod config

--- .config/nvim/lua/plugins/dadbod.lua
+++ .config/nvim/lua/plugins/dadbod.lua
@@ -1,33 +1,20 @@
--- Manage and query databases directly from Neovim
 ---@type LazySpec[]
 return {
-  {
-    "tpope/vim-dadbod",
-    -- keys = { { 'gx', '<cmd>Browse<cr>', mode = { 'n', 'x' } } },
-    -- dependencies = { 'nvim-lua/plenary.nvim' },
-    cmd = { "DB" },
-    lazy = true,
-    opts = {},
-  },
-  {
+  "tpope/vim-dadbod",
+  cmd = { "DB" },
+  dependencies = {
     "kristijanhusak/vim-dadbod-ui",
-    dependencies = {
-      { "tpope/vim-dadbod", lazy = true },
-      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true }, -- Optional
-    },
-    cmd = {
-      "DBUI",
-      "DBUIToggle",
-      "DBUIAddConnection",
-      "DBUIFindBuffer",
-    },
-    init = function()
-      -- Your DBUI configuration
-      vim.g.db_ui_use_nerd_fonts = true
-      vim.g.db_ui_execute_on_save = false
-      vim.g.db_ui_disable_mappings = true
-      vim.g.db_ui_save_location = "$HOME/code/sql/queries"
-      vim.g.db_ui_use_nvim_notify = true
-    end,
   },
+  keys = {
+    { "<leader>do", ":DBUI<CR><CR>",  desc = "Database UI Open" },
+    { "<leader>dc", ":DBUIClose<CR>", desc = "Database UI Close" },
+  },
+  config = function()
+    -- Your DBUI configuration
+    vim.g.db_ui_use_nerd_fonts = true
+    vim.g.db_ui_execute_on_save = false
+    vim.g.db_ui_disable_mappings = true
+    vim.g.db_ui_save_location = "$HOME/code/sql/queries"
+    vim.g.db_ui_use_nvim_notify = true
+  end,
 }
