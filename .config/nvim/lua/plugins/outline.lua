06c985d4ed6cfbeb249f098307bdfd092640d03b
Ashley Austin <austin.ashley.r@gmail.com>
2025-05-15 18:38:02 -0700

    refactor(nvim): add outline.nvim

---
+++ .config/nvim/lua/plugins/outline.lua
@@ -0,0 +1,26 @@
+return {
+  "hedyhli/outline.nvim",
+  keys = { { "<leader>cs", "<cmd>Outline<cr>", desc = "Toggle Outline" } },
+  cmd = "Outline",
+  opts = function()
+    local defaults = require("outline.config").defaults
+    local opts = {
+      symbols = {
+        icons = {},
+        filter = vim.deepcopy(aa.icons.kind_filter),
+      },
+      keymaps = {
+        up_and_jump = "<up>",
+        down_and_jump = "<down>",
+      },
+    }
+
+    for kind, symbol in pairs(defaults.symbols.icons) do
+      opts.symbols.icons[kind] = {
+        icon = aa.icons.kinds[kind] or symbol.icon,
+        hl = symbol.hl,
+      }
+    end
+    return opts
+  end,
+}
