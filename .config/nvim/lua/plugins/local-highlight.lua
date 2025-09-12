1c1f95a9161e893829b0414202e0cd674475945c
Ashley Austin <austin.ashley.r@gmail.com>
2025-09-12 13:01:30 -0700

    chore(nvim): remove deprecated plugins
    Remove unused plugins:
    - local-highlight.lua (commented out functionality)
    - lualine/init.lua and lualine/setup.lua (consolidated)
    - menu.lua (unused menu functionality)
    - neogen.lua (documentation generation)
    - outline.lua (symbol outline functionality)

--- .config/nvim/lua/plugins/local-highlight.lua
+++
@@ -1,6 +0,0 @@
--- vim.cmd [[hi TSDefinitionUsage guibg=#565f89]]
-
-return {
-  -- "tzachar/local-highlight.nvim",
-  -- config = true,
-}
--- .config/nvim/lua/plugins/neogen.lua
+++
@@ -1,5 +0,0 @@
-return {
-  "danymat/neogen",
-  keys = require("config.keymaps").neogen_mappings,
-  opts = { snippet_engine = "luasnip" },
-}
