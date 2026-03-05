e2ecf7fd662f0915022eab041ab529ce2616534b
Ashley Austin <austin.ashley.r@gmail.com>
2026-03-04 22:39:30 -0800

    fix(shell): fix brewb alias and fzf shell integration
    - Fix brewb alias to cd into brew config dir before running bundle
    - Use fzf built-in shell integration via `fzf --zsh` instead of
      sourcing raw scripts from GitHub
    - Remove command-history-preview and simplify fzf ctrl-r options
    - Move zvm_after_init keybinding to zinitrc alongside zsh-vi-mode

--- .config/zsh/fzf.zsh
+++ .config/zsh/fzf.zsh
@@ -4,13 +4,10 @@
 export FZF_DEFAULT_COMMAND='fd --type f --follow --hidden --color=always --exclude .git --exclude node_modules --exclude vendor --exclude build --exclude _build --exclude bundle --exclude Godeps'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_R_OPTS="
-  --preview 'command-history-preview {}'
-  --bind '?:toggle-preview,ctrl-a:select-all,ctrl-d:preview-page-down,ctrl-u:preview-page-up'
-  --bind 'ctrl-/:toggle-preview'
+  --bind 'ctrl-a:select-all,ctrl-d:preview-page-down,ctrl-u:preview-page-up'
   --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'
   --color header:italic
-  --header 'ctrl-y: copy, ctrl-/: toggle preview'
-  --preview-window down:20%:wrap
+  --header 'ctrl-y: copy'
   --height 50%"
 
 # add support for Ansi for fd color
@@ -24,8 +21,3 @@ export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
   --color=info:#88909f,prompt:#ec7279,pointer:#d38aea
   --color=marker:#a0c980,spinner:#ec7279,header:#5dbbc1'
 
-# zsh-vi-mode needs to rebind ctrl-r to make it available in insert mode
-# https://github.com/jeffreytse/zsh-vi-mode/issues/242
-function zvm_after_init() {
-  zvm_bindkey viins "^R" fzf-history-widget
-}
