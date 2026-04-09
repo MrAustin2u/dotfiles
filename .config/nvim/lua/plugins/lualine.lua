d09d3bca7a656a2e8830f4ea91f1e0e4ca2dca23
Ashley Austin <austin.ashley.r@gmail.com>
2026-04-09 02:07:53 -0700

    🎨 style(nvim): revert lualine filename to absolute path with tilde
    Switch back to `path = 3`. Full path context is more useful than just
    filename + parent dir when jumping around a large tree.

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -33,7 +33,7 @@ return {
         },
         lualine_b = { "branch", "diff", "diagnostics" },
         lualine_c = {
-          { "filename", path = 4 },
+          { "filename", path = 3 },
         },
         lualine_x = {
           {
